87ca8093b2318ab7b96d83c4a9c38b92064b8826
josiasmartins <josiasmartinscaitano@gmail.com>
2025-03-13 11:49:21 -0300

    feature: script powershell with azure

---
+++ script02.ps1
@@ -0,0 +1,18 @@
+# Conecta na conta  azure
+# Connect-AzAccount
+
+$Location="North Europe"
+$ResourceGroupName="web-grp"
+
+New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+
+$AppServicePlanName="learningplan7000"
+
+# Cria o App service plan
+New-AzAppServicePlan -ResourceGroupName $ResourceGroupName -Name $AppServicePlanName `
+-Location $Location -Tier Free -Linux
+
+$ApppName="learningapp58878777"
+
+New-AzWebApp -ResourceGroupName $ResourceGroupName -Location $Location -Name $ApppName `
+-AppServicePlan $AppServicePlanName
